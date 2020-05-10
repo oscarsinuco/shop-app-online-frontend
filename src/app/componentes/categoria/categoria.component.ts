7a1bbd894e7561f551b265ba7e73fbe287e87eb5
Oscar Esneyder <oscarsinuco@gmail.com>
2020-05-10 13:40:39 -0500

    categorias de BD

--- src/app/componentes/categoria/categoria.component.ts
+++ src/app/componentes/categoria/categoria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-categoria',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categoria.component.scss'],
 })
 export class CategoriaComponent implements OnInit {
-
+  @Input() nombre: string;
+  @Input() imagen: string;
   constructor() { }
 
   ngOnInit() {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PeticionesService } from '../peticiones.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,16 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
   marcas = {
-    breakpoints:{
-      0:{
+    breakpoints: {
+      0: {
         slidesPerView: 1.8,
         spaceBetween: 10
       },
-      500:{
+      500: {
         slidesPerView: 3.5,
         spaceBetween: 40
       },
-      1000:{
+      1000: {
         slidesPerView: 4.5,
         spaceBetween: 50
       },
@@ -31,16 +32,16 @@ export class Tab1Page {
     }
   };
   categorias = {
-    breakpoints:{
-      0:{
+    breakpoints: {
+      0: {
         slidesPerView: 4.4,
         spaceBetween: 10
       },
-      500:{
+      500: {
         slidesPerView: 4.4,
         spaceBetween: 2
       },
-      1000:{
+      1000: {
         slidesPerView: 7.5,
         spaceBetween: 50
       },
@@ -54,6 +55,18 @@ export class Tab1Page {
       slideShadows: true,
     }
   }
-  constructor() {}
+  public categoriasArray: any;
+  constructor(private peticiones: PeticionesService) {
+    this.peticiones.getCategorias().then(response => {
+
+      return response.json()
+
+    }).then(data => {
+
+      console.log(data)
+      this.categoriasArray = data['categorias']
+      
+    })
+  }
 
 }
