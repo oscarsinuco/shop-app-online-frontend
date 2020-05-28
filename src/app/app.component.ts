9264832258cdef14c460c18feab3e7a75ea759c5
Oscar Esneyder <oscarsinuco@gmail.com>
2020-05-28 12:29:54 -0500

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,5 @@ export class AppComponent {
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
   }
 }
